94aa57a9180d002c9f4d616c93ca5f9d2688d3cc
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-08-01 19:53:38 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -9,6 +9,6 @@ describe "current_age_for_birth_year method" do
   it "returns the age of a person born in the year 1922" do
     age_of_person = current_age_for_birth_year(1922)
 
-    expect(age_of_person).to eq(17)
+    expect(age_of_person).to eq(81)
   end
 end
